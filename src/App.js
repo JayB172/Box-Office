47c6a1dc23a940d504461f2f36090f42003a260e
jay <jay172bhat@gmail.com>
2021-01-06 09:20:58 +0530

    Used Navigation using React routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
+import React from "react";
+import { Switch, Route} from "react-router-dom";
+import Navs from "./Components/Navs"
+import Home from "./Pages/Home";
+import Starred from "./Pages/Starred";
 
 function App() {
   return (
     // eslint-disable-next-line react/react-in-jsx-scope
     <div>
-     hi
+      <Navs />
+      <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+    </Switch>
     </div>
+    
   );
 }
 
